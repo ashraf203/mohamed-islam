de93850166324c467be9032e7df0d5e24ced28ef
ashraf203 <sageddelivery@gmail.com>
2026-06-14 20:00:29 +0300

    الابلكيشن

---
+++ print.php
@@ -0,0 +1,12 @@
+<?php
+$name = "mohamed";
+$id = 5;
+$price = 10.5;
+$status = false; // false
+$order = null;
+$activities = ["gym", "football"];
+$hobbies = (object)["gem" . "football"];
+
+print_r($activities);
+// var_dump($hobbies);
+echo($name); // strings 
