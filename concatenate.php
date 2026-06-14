2ae49586894d1b72c08c04159dd098d09c6bef56
ashraf203 <sageddelivery@gmail.com>
2026-06-14 21:11:19 +0300

    Create concatenate.php

---
+++ concatenate.php
@@ -0,0 +1,18 @@
+<?php
+
+$first = "mohamed";
+
+$last = "islam";
+
+$age = 20;
+
+// $activities = ['running', 'reading'];
+// echo $first;
+// echo $" "
+// echo $last;
+
+echo "her name is" . $first . " " . $last . " and her age is $age";
+
+$message = "her name is $first $last <br> and her age is $age";
+
+// echo $message;
