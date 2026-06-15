1647117d632ccef72f2d1bd6ce819563e940bb58
ashraf203 <sageddelivery@gmail.com>
2026-06-16 01:47:10 +0300

    asd

---
+++ constants.php
@@ -0,0 +1,5 @@
+<?php
+
+define('namberOFProudcts', 10);
+
+echo namberOFProudcts;
---
+++ operators.php
@@ -0,0 +1,45 @@
+<?php
+
+# arithmetic operators (+ , - , * , / , ** , %)
+$firstNumber = 15;
+$secondNumber = 4;
+$result = $firstNumber % $secondNumber;
+// echo $result;
+// echo $firstNumber + $seconNumder;
+
+# assignments operators (= , += , -= , *= , /= , %= , .=)
+$name = "arwa";
+$name = $name . " mohamed";
+$name = "$name mohamed";
+$name .= " mohamed";
+// echo $name;
+$name = 5;
+// $number = $number += 2;
+// $number += 2;
+// echo $number;
+# comparison operators ( == , != , > , < , >= , <= ,=== , !==)
+# logical operators ( OR ||, AND &&, !)
+# increment / decrement operators
+
+# increment / decrement operators
+// $x = 5;
+// echo $x;
+// echo "<br>";
+// echo --$x; // post-increment
+// echo "<br>";
+// echo --$x; // pre -increment
+// echo "<br>";
+// echo $x;
+
+
+# object operator
+// -> , js->
+#array operator
+// =>
+# escape operator
+echo "he didn't answer";
+echo "he didn\'t answer";
+#scope resoultion operator
+// ::
+# ternary operator
+// ? :
