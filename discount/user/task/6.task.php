464602bc4e6b6a6e4ec28e37beaf41e8794f99cf
ashraf203 <sageddelivery@gmail.com>
2026-06-27 17:08:59 +0300

    s

--- discount/user/task/6.task.php
+++ discount/user/task/6.task.php
@@ -5,6 +5,18 @@
 
 <?php
 
-if($_POST) {
-    $number = trim($_post)
+if ($_POST) {
+    $number = floatval(trim($_POST['number'] ?? ''));
+    if ($number < 50) {
+        $cost = $number * 0.5;
+    } elseif ($number >= 50 && $number < 100) {
+        $cost = 0.75 * $number;
+    } elseif ($number >= 100 && $number < 150) {
+        $cost = 1.2 * $number;
+    } elseif ($number >= 150) {
+        $cost = 1.5 * $number;
+    } else {
+        $cost = 0;
+    }
+    echo $cost;
 }
